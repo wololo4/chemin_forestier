8790205155e29afe3449a23a8013fe055611e22e
wololo4 <59884953+wololo4@users.noreply.github.com>
2021-04-18 15:17:07 -0400

    Add files via upload

--- js/initCarte.js
+++ js/initCarte.js
@@ -2,23 +2,42 @@
 var map;
 var drawnItems;
 var osm2, osmAttrib;
-var MiniMap
+var MiniMap;
 
 //fonction qui permet d'initialiser la carte
 function initCarte(){
 
-    //declaration de la variable map
-    map = L.map("map", {center: [45.52,-73.63], zoom: 5});
 
     //declaration des variable de fond de carte
     osmUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     osmAttrib = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 
-    //ajout des fonds de carte
-    L.tileLayer(
+    var BING_KEY = 'rqqz4TIOHL36kTs5EI1l~OknGli9JCFEF98o18sh7ow~Ai0PB2EU5ZW4V3qu95fN0g98QXAwG6E2QmwryHjV7tJpOAiHQyKuwqrJaWM2ZAAL'
+
+    //fond de carte openstreetmap
+    var openstreetmap = new L.tileLayer(
         osmUrl, 
         {attribution: osmAttrib}
-    ).addTo(map);
+    )
+
+    //fond de carte bing map satellite
+    var bingMapsSatellite = L.tileLayer.bing(BING_KEY)
+
+	//declaration de la variable map
+    map = L.map("map", {
+        center: [45.52,-73.63], 
+        zoom: 5,
+        layers: [openstreetmap]
+    });
+
+    //groupe layer de fond de care
+	var baseLayers = {
+		"OpenStreetMap": openstreetmap,
+		"BingMapsSatellite": bingMapsSatellite
+	};
+
+    //ajout d'un groupe layer. permet de changer de fond de carte
+	L.control.layers(baseLayers).addTo(map);
 
     // Create feature group for drawn items & layer group for previously drawn items
     drawnItems = L.featureGroup().addTo(map);
